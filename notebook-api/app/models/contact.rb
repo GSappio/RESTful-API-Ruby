906512570607bf7282e3620aa1f4b78e9ae284a6
GabrielSappio <gabriel.sappio02@gmail.com>
2023-02-09 11:40:10 -0300

    Introduction to Rails API-Only and some basic concepts #02

--- notebook-api/app/models/contact.rb
+++ notebook-api/app/models/contact.rb
@@ -1,6 +1,27 @@
 class Contact < ApplicationRecord
+
+    # Associations
     belongs_to :kind #, optional: true
-    
+    has_many :phones
+
+    def as_json(options = {})
+        h = super(options)
+        h[:birthdate] = (I18n.l(self.birthdate) unless self.birthdate.blank?)
+        h
+    end
+
+    # def birthdate_br
+    #     I18n.l(self.birthdate) unless self.birthdate.blank?
+    # end
+
+    # def to_br
+    #     { 
+    #         name: self.name,
+    #         email: self.email,
+    #         birthdate: (I18n.l(self.birthdate) unless self.birthdate.blank?) 
+    #     } 
+    # end
+
     # def author
     #     "Gabriel Sappio"
     # end
@@ -15,4 +36,13 @@ class Contact < ApplicationRecord
     #         methods: [:kind_description, :author]
     #         )
     # end
-end
+
+    # def hello
+    #     I18n.t('hello')
+    # end
+    
+    # def i18n
+    #     I18n.default_locale
+    # end
+
+end 
