4707facf89d299bdc525c4afbbc2aaf9044fbcfe
GabrielSappio <gabriel.sappio02@gmail.com>
2023-02-14 11:43:06 -0300

    Creating a Middleware Rack

---
+++ notebook-api/lib/app_name.rb
@@ -0,0 +1,25 @@
+# class AppName
+#     def initialize(app)
+#         @app = app
+#     end
+
+#     def call(env)
+#         ['200', {'Content-Type' => 'text/html'}, ["Notebook API"]]
+#     end
+# end
+
+class AppName
+    def initialize(app, app_name)
+        @app = app
+        @app_name = app_name
+    end
+
+    def call(env)
+        puts ">>>>>>> #{env}"
+        if env["ORIGINAL_FULLPATH"] == "/"
+            ['200', {'Content-Type' => 'text/html'}, ["Teste!"]]
+        status, headers, response = @app.call(env)
+        headers.merge!({'X-App-Name' => "#{@app_name}"})
+        [status, headers, [response.body]]
+    end
+end
