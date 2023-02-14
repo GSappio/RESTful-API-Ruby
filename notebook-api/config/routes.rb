a5d6f7377af4eeb709de619b4b6332eb982b97d3
GabrielSappio <gabriel.sappio02@gmail.com>
2023-02-14 08:18:19 -0300

    Demystifying Accept and Content-Type

--- notebook-api/config/routes.rb
+++ notebook-api/config/routes.rb
@@ -35,6 +35,6 @@ Rails.application.routes.draw do
         resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
       end
     end
-  end 
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
