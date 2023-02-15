fddf505c0014ed8385bffb54f43eb6c9bb5889b3
GabrielSappio <gabriel.sappio02@gmail.com>
2023-02-15 08:31:19 -0300

    Improving a Hash class

--- notebook-api/spec/controllers/v1/contacts_spec.rb
+++ notebook-api/spec/controllers/v1/contacts_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
-# ARRUMAR O METODO DEPOIS
+
+class Hash
+     def json(parts)
+         ary = parts.split(">")
+          ary.reduce(self) do |memo, key|
+             memo.fetch(key.to_s.strip) if memo
+      end
+     end
+   end
+
+# ARRUMAR O METODO DEPOIS --> xdescribe
 xdescribe V1::ContactsController, type: :controller do
 
     it 'request index and return 406 NOT ACCEPTABLE' do
@@ -18,6 +28,9 @@ xdescribe V1::ContactsController, type: :controller do
         request.accept = 'application/vnd.api+json'
         get :show, params: { id: contact.id}
         response_body = JSON.parse(response.body)
-        expect(response_body.fetch('data').fetch('id')).to eq(contact.id.to_s)
+        expect(response_body.json('data > id')).to eq(contact.id.to_s)
+        expect(response_body.json('data > type')).to eq('contacts')
+        expect(response_body.json('data > attributes > name')).to eq(contact.name)
+
     end
 end
